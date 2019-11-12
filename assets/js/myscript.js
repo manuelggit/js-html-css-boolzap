a0192f8ef4e1ca00e007e50d42e063c78df35b58
Manuel Guerrini <man.guerrini@gmail.com>
2019-11-12 19:03:52 +0100

    JS - stampo un rettangolo verde in pagina

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -7,6 +7,16 @@ $( document ).ready(function(){
     var messaggio = $('.messaggio-utente').val();
     console.log(messaggio);
 
+    // copio il div del messaggio inviato che è all'interno del template: ne creo una variabile
+    var messaggioInviato = $('#template .messaggio-inviato').clone();
+    console.log(messaggioInviato);
+
+    // ora inserisco il testo del messaggio a questa copia
+    messaggioInviato.find('.messaggio-utente').text(messaggio);
+
+    // grazie all'append posso stampare in pagina (nel contenuto della chat) questo messaggio
+    $('#chat-contenuto').append(messaggioInviato);
+
   });
 
 });
