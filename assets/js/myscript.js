91a4949d4e6cd63157b619bc537e7e8298dd7026
Manuel Guerrini <man.guerrini@gmail.com>
2019-11-13 18:01:44 +0100

    cambio icone da microfono a aeroplano e viceversa quando invio un messaggio

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,7 +1,15 @@
 $( document ).ready(function(){
 
+  // quando clicco per scrivere un messaggio il microfono diventa aeroplano
+  $('.spazio-scrivi-messaggio').click(function(){
+    $('#bottone-invio').removeClass('fa-microphone').addClass('fa-paper-plane');
+  });
+
   // agganciamo al click sul pulsante microfono la funzione di callback
   $('.invio').click(function() {
+
+    // quando invio il messaggio l'aeroplano torna microfono
+    $('#bottone-invio').removeClass('fa-paper-plane').addClass('fa-microphone');
 
     // creo la variabile messaggio (quello che l'utente scrive)
     var messaggio = $('.messaggio-utente').val();
@@ -15,21 +23,14 @@ $( document ).ready(function(){
     messaggioInviato.removeClass('template');
     messaggioInviato.addClass('template-inviato');
 
-    // // risposta 'ok' (logica analoga al messaggio inviato)
-    // var contromessaggio = 'ok';
-    // console.log(contromessaggio);
-    //
-    // var messaggioRicevuto = $('.template .template-messaggio').clone();
-    // console.log(messaggioRicevuto);
-    // messaggioRicevuto.removeClass('template');
-    // messaggioRicevuto.addClass('template-ricevuto');
-
     // ora inserisco il testo del messaggio a questa copia
     messaggioInviato.find('.testo').text(messaggio);
     // messaggioRicevuto.text(contromessaggio);
 
     // grazie all'append posso stampare in pagina (nel contenuto della chat) questo messaggio
     $('#chat-contenuto').append(messaggioInviato);
+
+    // invoco la funzione di risposta dopo 1 secondo
 
     setTimeout(risposta, 1000);
 
@@ -45,9 +46,6 @@ $( document ).ready(function(){
       messaggioRicevuto.text(contromessaggio);
       $('#chat-contenuto').append(messaggioRicevuto);
     }
-
-
-
 
     // ripulisco il campo del messaggio-utente
     $('.messaggio-utente').val("");
