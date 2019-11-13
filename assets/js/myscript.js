e5ad1e364e385ea443ef4b4b8eb0278fe424e6c9
Manuel Guerrini <man.guerrini@gmail.com>
2019-11-13 15:34:24 +0100

    faccio comparire immediatamente l'ok di risposta

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -8,14 +8,28 @@ $( document ).ready(function(){
     console.log(messaggio);
 
     // copio il div del messaggio inviato che è all'interno del template: ne creo una variabile
-    var messaggioInviato = $('#template .template-messaggio').clone();
+    var messaggioInviato = $('.template .template-messaggio').clone();
     console.log(messaggioInviato);
+
+    // assegno una classe al messaggio inviato così da attribuirgli lo stile
+    messaggioInviato.removeClass('template');
+    messaggioInviato.addClass('template-inviato');
+
+    var contromessaggio = 'ok';
+    console.log(contromessaggio);
+
+    var messaggioRicevuto = $('.template .template-messaggio').clone();
+    console.log(messaggioRicevuto);
+    messaggioRicevuto.removeClass('template');
+    messaggioRicevuto.addClass('template-ricevuto');
 
     // ora inserisco il testo del messaggio a questa copia
     messaggioInviato.find('.testo').text(messaggio);
+    messaggioRicevuto.text(contromessaggio);
 
     // grazie all'append posso stampare in pagina (nel contenuto della chat) questo messaggio
     $('#chat-contenuto').append(messaggioInviato);
+    $('#chat-contenuto').append(messaggioRicevuto);
 
     // ripulisco il campo del messaggio-utente
     $('.messaggio-utente').val("");
